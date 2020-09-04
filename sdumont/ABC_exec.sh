11cb98591a15a11f3542fc57f77f7f0ce60bc26e
joaopedro-vm <joaopedrovm98@gmail.com>
2020-09-04 17:43:27 -0300

    SDumont Job Submission File

---
+++ sdumont/ABC_exec.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --nodes=1                      #Numero de Nós
+#SBATCH --ntasks-per-node=1            #Numero de tarefas por Nó
+#SBATCH --ntasks=1                     #Numero total de tarefas MPI
+#SBATCH --cpus-per-task=1              #Numero de threads
+#SBATCH -p cpu_dev                     #Fila (partition) a ser utilizada
+#SBATCH -J teste                       #Nome job
+#SBATCH --exclusive                    #Utilização exclusiva dos nós durante a execução do job
+
+mpiexec -n 1 python ABC_exec.py
